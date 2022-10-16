bc55a2a3af07e00fe74ddb71633ae0c46b097999
Rutvik-Kalathiya <112853725+Rutvik-Kalathiya@users.noreply.github.com>
2022-10-16 13:25:53 +0200

    Added Movie Component - Use Bootstrap and Font-awesome css - Load Movie Component from FakeMovieService

---
+++ src/components/movies.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { getMovies } from "../services/fakeMovieService";
+
+function Movies() {
+  const [movies, setMovies] = useState(getMovies);
+  const handleDelete = (movie) => {
+    setMovies(movies.filter((m) => m._id !== movie._id));
+  };
+  if (movies.length === 0) return <p>There are no movies in the Database</p>;
+  return (
+    <div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Genre</th>
+            <th>Stock</th>
+            <th>Rate</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {movies.map((movie) => (
+            <tr key={movie._id}>
+              <td>{movie.title}</td>
+              <td>{movie.genre.name}</td>
+              <td>{movie.numberInStock}</td>
+              <td>{movie.dailyRentalRate}</td>
+              <td>
+                <button
+                  onClick={() => handleDelete(movie)}
+                  className="btn btn-danger btn-sm"
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Movies;
