8a7cc72a95042f271eca5aa7e7efeba0ee798df7
Rutvik-Kalathiya <112853725+Rutvik-Kalathiya@users.noreply.github.com>
2022-10-16 15:02:00 +0200

    Added counter component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Counters from './components/counters';
 import Movies from './components/movies';
 
 function App() {
   return (
       <main className="container">
         <Movies />
+        <Counters />
       </main>
   );
 }
---
+++ src/components/counters.jsx
@@ -0,0 +1,5 @@
+function Counters() {
+  return <h1>This is a counter component</h1>;
+}
+
+export default Counters;
