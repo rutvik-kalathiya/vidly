7c8532869c4dcb55068dd0c233dfbfe9d49c642d
Rutvik <>
2022-11-09 00:41:28 +0100

    Addded Movie component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
 import CounterClassComponent from './components/counterClassComponent';
 import CounterFunctionalComponent from './components/counterFunctionalComponent';
+import MoviesClassComponent from './components/moviesClassComponent';
+import MoviesFunctionalComponent from './components/moviesFunctionalComponent';
 
 function App() {
   return (
       <main className="container">
         <CounterClassComponent />
         <hr />
+
         <CounterFunctionalComponent />
+        <hr />
+
+        <MoviesClassComponent />
+        <hr />
+
+        <MoviesFunctionalComponent />
       </main>
   );
 }
